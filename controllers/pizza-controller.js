ae63a2194ab4f1e8ea28e8a8db815e548c77428b
elfsvet <stepanmatysik@gmail.com>
2022-07-03 23:20:16 -0400

    added and updated the calls added select sort and populate

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,6 +5,14 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+        // to display like include in sql call to see more information in the api call
+            .populate({
+                path: 'comments',
+                // dont want it to return we use minus. without it it will return only the select.
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1})
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -15,6 +23,11 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
                 // If no pizza is found, send 404
                 if (!dbPizzaData) {
@@ -49,16 +62,16 @@ const pizzaController = {
     },
 
     // delete pizza
-    deletePizza({params}, res) {
-        Pizza.findOneAndDelete({_id: params.id})
-        .then(dbPizzaData => {
-            if(!dbPizzaData) {
-                res.status(404).json({message: 'No pizza found with this id!'});
-                return;
-            }
-            res.json(dbPizzaData);
-        })
-        .catch(err => res.status(400).json(err));
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
     }
 };
 
--- models/Pizza.js
+++ models/Pizza.js
@@ -15,7 +15,26 @@ const PizzaSchema = new Schema({
         type: String,
         default: 'Large'
     },
-    toppings: [] // could've use Array
+    toppings: [], // could've use Array
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+// add options:
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
